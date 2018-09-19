3dd8a176674bd03db6f923f6d37f9cd0f5194706
mwai <wangenga.peter@gmail.com>
2018-09-19 11:55:52 +0300

    request validation routes

--- app/Http/Requests/RegistrationRequest.php
+++ app/Http/Requests/RegistrationRequest.php
@@ -24,7 +24,8 @@ class RegistrationRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6|max:10'
         ];
     }
 }
