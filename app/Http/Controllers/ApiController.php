7f9d85809284a8357e0fbf5abda7052799dc81aa
mwai <wangenga.peter@gmail.com>
2018-09-19 11:58:25 +0300

    setup user registration

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegistrationRequest;
+use App\User;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public $loginAfterSignUp = true;
+
+    public function register(RegistrationRequest $request)
+    {
+        $user = new User();
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+        
+
+        return response()->json([
+            'success' => true,
+            'data' => $user
+        ], 200);
+    }
 }
