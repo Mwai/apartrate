bb06b460085a351d0f5a02d5a36c5523a1fd2f08
mwai <wangenga.peter@gmail.com>
2018-09-19 12:34:44 +0300

    created the genre model

---
+++ app/Genre.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Genre extends Model
+{
+    protected $table = 'genres';
+    protected $guarded = ['id'];
+
+    public function films()
+    {
+        return $this->belongsToMany('App\Film');
+    }
+
+}
