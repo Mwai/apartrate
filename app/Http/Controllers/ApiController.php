903de7595f590b9bcef9361af0713907f4506c18
mwai <wangenga.peter@gmail.com>
2018-09-19 12:00:25 +0300

    setup login logic and allow user to login after registration

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,12 +16,32 @@ class ApiController extends Controller
         $user->email = $request->email;
         $user->password = bcrypt($request->password);
         $user->save();
-
         
+        if ($this->loginAfterSignUp) {
+            return $this->login($request);
+        }
 
         return response()->json([
             'success' => true,
-            'data' => $user
+            'data'    => $user,
         ], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $input = $request->only('email', 'password');
+        $jwtToken = JWTAuth::attempt($input);
+
+        if (!$jwtToken) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Email or Password',
+            ], 401);
+        }
+
+        return response()->json([
+            'success' => true,
+            'token'   => $jwtToken,
+        ]);
     }
 }
