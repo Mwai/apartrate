ffddd991f7c79c0af973d243543b5bd8f97949cc
mwai <wangenga.peter@gmail.com>
2018-09-19 12:01:56 +0300

    user logout logic

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,7 +16,7 @@ class ApiController extends Controller
         $user->email = $request->email;
         $user->password = bcrypt($request->password);
         $user->save();
-        
+
         if ($this->loginAfterSignUp) {
             return $this->login($request);
         }
@@ -43,5 +43,25 @@ class ApiController extends Controller
             'success' => true,
             'token'   => $jwtToken,
         ]);
+    }
+    public function logout(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+
+        try {
+            JWTAuth::invalidate($request->token);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User logged out successfully'
+            ]);
+        } catch (JWTException $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, the user cannot be logged out'
+            ], 500);
+        }
     }
 }
