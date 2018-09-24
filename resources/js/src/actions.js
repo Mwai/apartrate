be5f3e358d5d0a37eb3e8b522965ab495984e46d
mwai <wangenga.peter@gmail.com>
2018-09-24 10:48:23 +0300

    action to load films

--- resources/js/src/actions.js
+++ resources/js/src/actions.js
@@ -1,13 +1,26 @@
 import * as types from './mutation_types'
 import request from './request'
+
 export const loginUser = ({commit}, {data}) => {
     request.post('/api/login/', data)
         .then(() => {
             return true
         })
         .catch(error => {
-            let errorMessage = error.request + ':Error Sending Reply'
-            //commit(types.ERROR_MESSAGE, errorMessage)
+            let errorMessage = error.request + ':Error Logging in'
+            commit(types.ERROR_MESSAGE, errorMessage)
             console.log(error)
         })
+}
+export const fetchFilms = ({commit}, {page}) => {
+    commit(types.LOADING)
+    request.get('/api/films?page=' + page).then((response) => {
+        commit(types.SET_FILMS, {list: response.data.data})
+        commit(types.LOADING)
+    }).catch(error => {
+        let errorMessage = error.request + ':Error loading films'
+        commit(types.ERROR_MESSAGE, errorMessage)
+        commit(types.LOADING)
+        console.log(error)
+    })
 }
