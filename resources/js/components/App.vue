5eee6a7e4d102c889de78890db9568feb6a79f50
mwai <wangenga.peter@gmail.com>
2018-09-24 09:16:52 +0300

    main app component

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <router-view :key="$route.fullPath" class="view"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
