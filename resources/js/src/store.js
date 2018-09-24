815c174f9c5ee2e0d93cb7ca90be0408222e096b
mwai <wangenga.peter@gmail.com>
2018-09-24 09:12:38 +0300

    add store with initial variables

---
+++ resources/js/src/store.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as getters from './getters'
+import * as actions from './actions'
+import mutations from './mutations'
+
+Vue.use(Vuex)
+let jwt = localStorage.getItem('auth_token'),
+    user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : {},
+    token_expiry = localStorage.getItem('token_expiry') ? localStorage.getItem('token_expiry') : null
+
+const state = {
+    user: user,
+    token_expiry: token_expiry,
+    loading: false,
+    authenticated: !!jwt,
+}
+export default new Vuex.Store({
+    state,
+    getters,
+    mutations,
+    actions
+})
