803b2300cb6bcc1f04cf3d851aea07786c358d32
mwai <wangenga.peter@gmail.com>
2018-09-24 09:06:11 +0300

    add token to all requests

---
+++ resources/js/src/request.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import * as types from './mutation_types'
+import auth from './auth'
+import moment from 'moment'
+
+
+if (localStorage.getItem('auth_token')) {
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('auth_token')
+}
+let instance = axios.create({
+    baseURL: '/'
+})
+
+instance.interceptors.response.use((response) => {
+    let token_expiry = store.state.token_expiry, current_date = moment().unix(),
+        expiry_date = moment(token_expiry).unix()
+    return response
+
+    if (typeof token_expiry === 'undefined' || !token_expiry || current_date > expiry_date) {
+        auth.logout()
+    } else {
+        return response
+    }
+}, function (error) {
+    // Do something with response error
+    if (error.response.status === 401) {
+        auth.logout()
+    }
+})
+
+export default instance
