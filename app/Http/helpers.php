7fa7d3da6631363a74f29919e8f8725197f8a27b
mwai <wangenga.peter@gmail.com>
2018-09-24 08:51:11 +0300

    fetch data from api helper

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -3,15 +3,17 @@
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\BadResponseException;
 
-function fetchApiResult($url, $method)
+function fetchApiResult($url, $method, $options = [])
 {
     $client = new Client();
     try {
-        $res = $client->request($method, $url);
+        $res = $client->request($method, $url, $options);
         $body = $res->getBody();
         $content = $body->getContents();
     } catch (BadResponseException $ex) {
         $response = $ex->getResponse();
+
+        dd($response);
         $content = (string) $response->getBody();
     }
 
