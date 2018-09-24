46912a8a968275cced744f367def23b92ddd048a
mwai <wangenga.peter@gmail.com>
2018-09-24 12:18:24 +0300

    loading animation

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,15 +1,23 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <router-view :key="$route.fullPath" class="view"/>
+            <div class="lds-dual-ring" v-if="getLoader"></div>
+            <router-view :key="$route.fullPath" class="view col-sm-12"/>
         </div>
     </div>
 </template>
 
 <script>
+    import {mapGetters} from 'vuex'
+
     export default {
         mounted() {
             console.log('Component mounted.')
+        },
+        computed: {
+            ...mapGetters([
+                'getLoader'
+            ])
         }
     }
 </script>
