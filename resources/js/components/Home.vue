9d677d2e71b7de1acdbf361b892f59d928ba9d6d
mwai <wangenga.peter@gmail.com>
2018-09-24 08:56:57 +0300

    initial home page

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Films</div>
+                    <router-link to="/films">
+                        View Films
+                    </router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
