f3328b2abb5d995042116824d1e86e6a669d6f59
mwai <wangenga.peter@gmail.com>
2018-09-26 15:54:11 +0300

    add mutations for user actions

--- resources/js/src/mutations.js
+++ resources/js/src/mutations.js
@@ -1,9 +1,14 @@
 import * as types from './mutation_types'
+import Vue from 'vue'
 
 export default {
 
     [types.SET_USER](state, {list}) {
-        state.user = list
+        localStorage.setItem('auth_token', list.token)
+        localStorage.setItem('user', JSON.stringify(list.user))
+        state.user = list.user
+        state.token = list.token
+        state.authenticated = true
     },
     [types.SET_FILMS](state, {list}) {
         state.films = list
@@ -16,5 +21,20 @@ export default {
     },
     [types.SET_CURRENT_FILM](state, {list}) {
         state.currentFilm = list
+    },
+    [types.LOGOUT_USER](state) {
+        localStorage.removeItem('auth_token')
+        localStorage.removeItem('user')
+        state.user = {}
+        state.authenticated = false
+    },
+    [types.SET_COMMENT](state, {list}) {
+        let comments = state.currentFilm.comments
+        comments.push(list)
+        Vue.set(state.currentFilm, 'comments', comments)
+    },
+    [types.SET_TOKEN](state, {list}) {
+        localStorage.setItem('auth_token', list)
+        Vue.set(state, 'token', list)
     },
 }
