d596d94780238e5988d0fa1a17f3299a3865ac57
mwai <wangenga.peter@gmail.com>
2018-09-26 15:53:50 +0300

    set authorisation header to each request

--- resources/js/src/request.js
+++ resources/js/src/request.js
@@ -1,31 +1,38 @@
 import axios from 'axios'
 import auth from './auth'
-import moment from 'moment'
 import store from './store'
 
-
-if (localStorage.getItem('auth_token')) {
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('auth_token')
-}
 let instance = axios.create({
     baseURL: '/'
 })
+instance.interceptors.request.use(
+    (config) => {
+        let token = localStorage.getItem('auth_token');
 
-instance.interceptors.response.use((response) => {
-    let token_expiry = store.state.token_expiry, current_date = moment().unix(),
-        expiry_date = moment(token_expiry).unix()
-    return response
+        if (token) {
+            config.headers['Authorization'] = 'Bearer ' + token
+        }
 
-    if (typeof token_expiry === 'undefined' || !token_expiry || current_date > expiry_date) {
-        auth.logout()
-    } else {
-        return response
+        return config;
     }
+);
+instance.interceptors.response.use((response) => {
+    checkResponseToken(response)
+    return response
 }, function (error) {
+    checkResponseToken(error.response)
     // Do something with response error
     if (error.response.status === 401) {
         auth.logout()
     }
 })
+
+export const checkResponseToken = (response) => {
+    const newToken = response.headers.authorization
+    if (newToken) {
+        let token = newToken.replace(/Bearer/g, '')
+        store.commit('SET_TOKEN', token)
+    }
+}
 
 export default instance
