32ff811d96c5adb908839bf26eef891ee4de91a8
mwai <wangenga.peter@gmail.com>
2018-09-26 15:56:56 +0300

    add navbar

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
+        <nav class="navbar navbar-light bg-white fixed-top">
+            <router-link to="/" class="navbar-brand">Generic Films</router-link>
+            <div v-if="checkAuthentication">
+                <a href="#" @click="logoutUser">
+                    <avatar :size="40" :username="getLoggedInUser.name"/>
+                    Logout
+                </a>
+            </div>
+        </nav>
+        <div class="row justify-content-center mt-5">
             <div class="lds-dual-ring" v-if="getLoader"></div>
             <router-view :key="$route.fullPath" class="view col-sm-12"/>
         </div>
@@ -16,8 +25,16 @@
         },
         computed: {
             ...mapGetters([
-                'getLoader'
+                'getLoader',
+                'getLoggedInUser',
+                'checkAuthentication'
             ])
+        },
+        methods: {
+            logoutUser() {
+                this.$store.dispatch('logoutUser')
+
+            }
         }
     }
 </script>
