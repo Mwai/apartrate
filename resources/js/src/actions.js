24fa026585cdca65f90e2aad1bb77ca127e1ac7d
mwai <wangenga.peter@gmail.com>
2018-10-01 07:49:13 +0300

    actions to fetch genres

--- resources/js/src/actions.js
+++ resources/js/src/actions.js
@@ -1,5 +1,6 @@
 import * as types from './mutation_types'
 import request from './request'
+import router from "./router";
 
 export const loginUser = ({commit}, {data}) => {
     commit(types.LOADING)
@@ -69,6 +70,51 @@ export const postComment = ({commit}, {data}) => {
     request.post('/api/comment/', data).then((response) => {
         commit(types.SET_COMMENT, {list: response.data.data})
         commit(types.LOADING)
+    }).catch(error => {
+        let errorMessage = error.request + ':Could not post comment'
+        commit(types.ERROR_MESSAGE, errorMessage)
+        commit(types.LOADING)
+        console.log(error)
+    })
+}
+export const fetchGenres = ({commit}) => {
+    commit(types.LOADING)
+    request.get('/api/genres/').then((response) => {
+        let results = response.data.data,
+            genres = []
+        results.forEach(function (genre) {
+            genres.push({
+                label: genre.name,
+                value: genre.id
+            })
+        })
+        commit(types.SET_GENRES, {list: genres})
+        request.get('https://restcountries.eu/rest/v1/all').then(function (response) {
+            let results = response.data,
+                countries = []
+            results.forEach(function (country) {
+                countries.push({
+                    label: country.name,
+                    value: country.name
+                })
+            })
+            commit(types.SET_COUNTRIES, {list: countries})
+            commit(types.LOADING)
+        })
+    }).catch(error => {
+        let errorMessage = error.request + ':Could not fetch genres'
+        commit(types.ERROR_MESSAGE, errorMessage)
+        commit(types.LOADING)
+        console.log(error)
+    })
+}
+export const postFilm = ({commit}, {data}) => {
+    commit(types.LOADING)
+    console.log(data)
+    request.post('/api/films/', data).then((response) => {
+        let film = response.data.data
+        commit(types.LOADING)
+        router.push('/films/' + film.slug)
     }).catch(error => {
         let errorMessage = error.request + ':Could not post comment'
         commit(types.ERROR_MESSAGE, errorMessage)
